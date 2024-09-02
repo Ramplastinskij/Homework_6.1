aec8d667958593753a8926a6aa4b5da720c52497
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-09-02 21:17:45 +0500

    Tasks solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Homework_6.1!");
+        //Task 1.
+        System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        //Task 2.
+        System.out.println();
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + upperCaseFullName);
+        //Task 3.
+        System.out.println();
+        firstName = firstName.replace ("Ivan", "Сёмен");
+        middleName = middleName.replace ("Ivanovich", "Сёменович");
+        lastName = lastName.replace ("Ivanov", "Иванов");
+        String fullNameNew = lastName + " " + firstName + " " + middleName;
+        String modifiedFullName = fullNameNew.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + modifiedFullName);
     }
 }
